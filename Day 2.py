302d7c5f04a886bcfb400e60da19cee2c399f96e
qkr0103 <qkrdlsdud0103@naver.com>
2025-05-20 03:20:37 +0900

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,48 @@
+# A,B = map(int,input().split())
+# if 0< A < 10 and 0< B < 10:
+#     print(A+B)
+# else:
+#     print("입력값 범위 벗어남")
+
+
+# A,B = map(int, input().split())
+# if 0 < A < 10 and 0 < B < 10:
+#     print(A - B)
+# else:
+#     print("입력값 범위 벗어남")
+
+
+# A,B = map(int, input().split())
+# if 0 < A < 10 and 0 < B < 10:
+#     print(A * B)
+# else:
+#     print("입력값 범위 벗어남")
+
+
+
+# A,B = map(int, input().split())
+# if 1<= A <= 10000 and 1<= B <= 10000:
+#     print(A + B)
+#     print(A - B)
+#     print(A * B)
+#     print(A / B)
+#     print(A % B)
+# else:
+#     print("입력값 범위 벗어남")
+
+
+
+# A,B = map(int, input().split())
+# if 0 < A < 10 and 0 < B < 10:
+    
+
+# A,B = map(int, input().split())
+# if 0 < A < 10 and 0 < B < 10:
+#     print(A/B)
+    
+
+
+# A,B,C = map(int, input().split())
+# if 1 <= A <= 10**12 and 1 <= B <= 10**12 and 1 <= C <= 10**12:
+#     print(A + B + C)
+    
