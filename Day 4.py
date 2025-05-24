39d50dc384ad31f6dfea8e9ac722bda94f50d1a7
qkr0103 <qkrdlsdud0103@naver.com>
2025-05-25 03:27:43 +0900

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,37 @@
+# N = int(input())
+# print(list(range(1,N+1)))
+
+
+# import math
+# N = int(input())
+# print(math.factorial(N))
+
+
+# T = int(input())
+# for _ in range(T):
+#     A,B = map(int,input().split())
+#     print(A+B)
+
+
+# N = int(input())
+# for i in range(1,10):
+#     print(f"{N}*{i}={N*i}")
+
+
+# N = int(input())
+# for i in range(1,N+1):
+#     print("*"*i)
+
+
+# TC = int(input())
+# for _ in range(TC):
+#     quiz = input()
+#     score = 0
+#     consecutive = 0
+#     for ch in quiz:
+#         if ch == 'O':
+#             consecutive += 1
+#             score += consecutive
+#         else:
+#             consecutive = 0
+#     print(score)
